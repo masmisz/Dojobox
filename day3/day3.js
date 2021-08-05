5e6e50bfa5b85ec21b2a30fe7bee7e1e2b63d8c4
masmisz <missioneto@gmail.com>
2021-08-05 20:26:18 +0700

    commit day4
    commit day4

--- day3/day3.js
+++ day3/day3.js
@@ -1,15 +1,35 @@
 //Function
-function jumlah2kubus(a,b){
-    function volumeKubus(s){
-     v = s * s * s;
-    return v;
+var inputSisiA = parseInt(prompt('Masukkan sisi kubus A ', '0-10'));
+var inputSisiB = parseInt(prompt('Masukkan sisi kubus B ', '0-10'));
+var volumeKubusA = hitungKubusA(inputSisiA);
+var volumeKubusB = hitungKubusA(inputSisiB);
+var jumlahKubus = penjumlahanKubus(volumeKubusA, volumeKubusB)
+document.write('Sisi kubus A = '+inputSisiA+'<br>')
+document.write('Sisi kubus B = '+inputSisiB+'<br>')
+document.write('Volume kubus A = '+volumeKubusA+'<br>')
+document.write('Volume kubus B = '+volumeKubusB+'<br>')
+document.write('Jumlah kedua volume kubus = '+jumlahKubus+'<br>')
+
+function hitungKubusA(sisiA){
+    var jumlah =1;
+    for (let index = 0; index < 3; index++) {
+        jumlah *= sisiA;
     }
-    var ka = volumeKubus(a);
-    var kb = volumeKubus(b);
-    var total = ka + kb;
-    
-    return total;
+    return jumlah;
 }
 
+function hitungKubusB(sisiB){
+    var jumlah = 1;
+    var count = 0;
+    while (count<3) {
+        jumlah *= sisiB;
+        count++;
+    }
+    return jumlah;
+}
 
-alert(jumlah2kubus(8,4));
+function penjumlahanKubus(volumeA, volumeB){
+    var jumlah = 0;
+    jumlah = volumeA+volumeB;
+    return jumlah;
+}
---
+++ day4/day4.js
@@ -0,0 +1,66 @@
+//Function
+// function tambah(a,b) {
+//     return a+b
+// }
+
+// document.write(tambah(5,3) + '<br>');
+// document.write(tambah(4) + '<br>');
+// document.write(tambah(5,2,5) + '<br>');
+
+
+// var penjumlahan = function(angka1,angka2) {
+//     var hasil
+//     hasil = angka1 + angka2
+//     return hasil
+// }
+
+// document.write(penjumlahan(3,4));
+
+
+// function cetak(bill1,bill2) {
+//     document.write('Tagihan pertama anda : Rp.' + bill1 + '<br>');
+//     document.write('Tagihan kedua anda : Rp.' + bill2 + '<br>');
+// }
+
+// cetak('200.000','350.000');
+
+
+//Expression
+// var penjumlahan = function(bilangan1, bilangan2) {
+//     return bilangan1 + bilangan2
+// }
+
+// document.write(penjumlahan(6,7));
+
+
+
+// // Literal declaration
+// //Object
+// var akademia = {
+//     //property
+//     nama : 'Masmisz',
+//     NIK : '9929292900110',
+//     Email : 'a@a.a',
+
+//     //method
+//     getName : function(){
+//         console.log('Nama saya adalah' + this.nama + '<br>')
+//     },
+//     getEmail : function(){
+//         console.log('Email saya adalah' + this.email + '<br>')
+//     },
+// }
+
+//Constructor
+function akademia(nama,nik,email) {
+    this.nama = nama;
+    this.email = email;
+    this.nik = nik;
+    
+}
+
+
+
+
+
+
