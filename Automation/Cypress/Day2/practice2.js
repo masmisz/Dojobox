47b4a432804b577f5aaefd41a6fa28d7df4d5d99
masmisz <44495771+masmisz@users.noreply.github.com>
2021-08-25 21:19:37 +0700

    Update practice2.js

--- Automation/Cypress/Day2/practice2.js
+++ Automation/Cypress/Day2/practice2.js
@@ -1,8 +1,8 @@
 describe('Tugas 2 (Cypress)', function () {
     it('email me', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/ ')
-        cy.get('#et_pb_contact_name_0').type('miswanto')
-        cy.get('#et_pb_contact_email_0').type('m@m.com')
+        cy.get('#et_pb_contact_name_0').type('miswanto').should('have.value','miswanto')
+        cy.get('#et_pb_contact_email_0').type('m@m.com').should('have.value','m@m.com')
         cy.get('.et_pb_contact_submit').click()
     })
 
@@ -13,8 +13,6 @@ describe('Tugas 2 (Cypress)', function () {
 
     it('checkboxes', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/ ')
-        // cy.get('input[type = "checkbox"]').check().should('be.checked').and('have.value', 'Bike')
-        // cy.get('input[type = "checkbox"]').uncheck().should('not.be.checked').and('have.value', 'Bike')
         cy.get('input[type = "checkbox"]').check('Bike')
         cy.get('input[type = "checkbox"]').check('Car')
     })
@@ -27,6 +25,7 @@ describe('Tugas 2 (Cypress)', function () {
     it('click me', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/ ')
         cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
+        cy.get('.entry-title').should('contain', 'Button success')
     })
 
     Cypress.on('uncaught:exception', (err, runnable) => {
