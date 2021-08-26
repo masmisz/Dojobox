608ae625d62d9082a5c5d72b5c583e941d37c5f8
masmisz <44495771+masmisz@users.noreply.github.com>
2021-08-26 21:25:01 +0700

    Update registrasi.js

--- Automation/Cypress/Day4/registrasi.js
+++ Automation/Cypress/Day4/registrasi.js
@@ -2,8 +2,8 @@ describe('Registrasi', function () {
     it('Registrasi', function () {
         cy.visit('https://www.demoblaze.com/index.html')
         cy.get('#signin2').click()
-        cy.get('#sign-username').type('dojo-mas1').should('have.value', 'dojo-mas1')
-        cy.get('#sign-password').type('dojo-mas1').should('have.value', 'dojo-mas1')
+        cy.get('#sign-username').type('dojo-mas').should('have.value', 'dojo-mas')
+        cy.get('#sign-password').type('dojo-mas').should('have.value', 'dojo-mas')
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
     })
 })
