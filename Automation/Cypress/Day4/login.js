fb8ba15dc32f9e64e8a61e0023e3447438dc457b
masmisz <44495771+masmisz@users.noreply.github.com>
2021-08-26 20:57:24 +0700

    Create login.js

---
+++ Automation/Cypress/Day4/login.js
@@ -0,0 +1,9 @@
+describe('Login', function () {
+    it('Login', function () {
+        cy.visit('https://www.demoblaze.com/index.html')
+        cy.get('#login2').click()
+        cy.get('#loginusername').type('dojo-mas').should('have.value', 'dojo-mas')
+        cy.get('#loginpassword').type('dojo-mas').should('have.value', 'dojo-mas')
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+    })
+})
