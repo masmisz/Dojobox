382e4dae6bb3d2865217a939ff01ea8e53d4d79a
masmisz <44495771+masmisz@users.noreply.github.com>
2021-08-30 22:09:19 +0700

    Update tugas5.js
    merapihkan code

--- Automation/Cypress/Day5/tugas5.js
+++ Automation/Cypress/Day5/tugas5.js
@@ -1,49 +1,39 @@
-describe('Login Demoblaze', function () {
+describe('Belanja Demoblaze', function () {
     it('Login with valid user', function () {
         cy.visit('https://www.demoblaze.com/index.html')
         cy.get('#login2').click()
-        cy.wait(1000)
-        cy.get('#loginusername').type('dojo-mas').should('have.value', 'dojo-mas')
-        cy.get('#loginpassword').type('dojo-mas').should('have.value', 'dojo-mas')
-        cy.get('button').contains('Log in').click()
-    })
+        cy.get('#loginusername', {timeout: 5000}).type('dojo-mas',{force:true})
+        cy.get('#loginpassword', {timeout: 5000}).type('dojo-mas')
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+        cy.get('#nameofuser').should('be.visible');
 
-    it('Beli HTC One M9', function () {
-        cy.visit('https://www.demoblaze.com/index.html')
         cy.get(':nth-child(7) > .card > .card-block > .card-title > .hrefch').click()
         cy.get('.col-sm-12 > .btn').click()
-        // cy.get('.active > .nav-link').click()
-        cy.go('back')
-        cy.go('back')
-        cy.wait(5000)
-    })
+        cy.get('#nava').click()
+        cy.wait(3000)
 
-    it('Beli Dell i7 8gb', function () {
         cy.get('#next2').click()
         cy.get(':nth-child(3) > .card > .card-block > .card-title > .hrefch').click()
         cy.get('.col-sm-12 > .btn').click()
         cy.go('back')
         cy.go('back')
-        // cy.get('.active > .nav-link').click()  
-        cy.wait(5000)
-    })
+        cy.wait(3000)
 
-    it('Beli Apple Monitor 24', function () {
         cy.get('#next2').click()
         cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click()
         cy.get('.col-sm-12 > .btn').click()
-        // cy.get('.active > .nav-link').click()  
-        cy.wait(5000)
-    })
- 
-    it('BAYAR', function () {
+
         cy.get('#cartur').click()
         cy.get('.col-lg-1 > .btn').click()
-        cy.get('#name').type('dojo-mas').should('have.value', 'dojo-mas')
+        cy.get('#name').type('dojo-mas',{force:true}).should('have.value', 'dojo-mas')
         cy.get('#city').type('Jakarta').should('have.value', 'Jakarta')
         cy.get('#card').type('123456789012345').should('have.value', '123456789012345')
         cy.get('#month').type('March').should('have.value', 'March')
         cy.get('#year').type('2026').should('have.value', '2026')
         cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+    })
+
+    Cypress.on('uncaught:exception', (err, runnable)=>{
+        return false
     })
 })
